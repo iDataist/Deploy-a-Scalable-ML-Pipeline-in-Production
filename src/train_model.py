bdd3606c59b27c59a98fd68d78c758ca81222edc
Hui Ren <huiren16@gmail.com>
2021-07-25 17:38:04 -0500

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,10 +1,14 @@
-# Script to train machine learning model.
-import joblib
-from ml.data import import_data, perform_eda, scaler, encoder, perform_train_test_split
+from ml.data import (
+    import_data,
+    perform_eda,
+    scaler,
+    encoder,
+    perform_train_test_split,
+)
 from ml.model import train_models, inference, compute_model_metrics
 import constants
-
 import logging
+
 logging.basicConfig(
     level=logging.INFO,
     filename=constants.log_path,
@@ -15,24 +19,16 @@ logger = logging.getLogger()
 logger.info("############################################################")
 logger.info("import data")
 df = import_data(constants.data_path)
-logger.info(
-    f"inspect dataframe: \
-    \n{df.iloc[0]}"
-)
+logger.info(f"inspect dataframe:     \n{df.iloc[0]}")
 logger.info(f"generate EDA report: {constants.eda_output_path}")
 perform_eda(df, constants.eda_output_path)
 logger.info(f"normalize numeric features: {constants.quant_columns}")
 df = scaler(df, constants.quant_columns)
-logger.info(
-    f"inspect dataframe: \
-    \n{df.iloc[0]}"
-)
+logger.info(f"inspect dataframe:     \n{df.iloc[0]}")
 logger.info(f"one-hot-encode categorical features:{constants.cat_columns}")
 df = encoder(df, constants.cat_columns)
-logger.info(
-    f"inspect dataframe: \
-    \n{df.iloc[0]}"
-)
+logger.info(f"inspect dataframe:     \n{df.iloc[0]}")
+df.to_csv(constants.processed_data_path, index=False)
 logger.info(
     f"perform train test split with the test size of {constants.test_size}"
 )
@@ -41,12 +37,13 @@ X_train, X_test, y_train, y_test = perform_train_test_split(
 )
 logger.info("start training")
 best_model, best_params = train_models(
-                                X_train,
-                                X_test,
-                                y_train,
-                                y_test,
-                                constants.image_output_path,
-                                constants.model_output_path)
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    constants.image_output_path,
+    constants.model_output_path,
+)
 logger.info(f"best parameters are: {best_params}")
 logger.info(
     f"save models in {constants.model_output_path}, "
@@ -55,4 +52,4 @@ logger.info(
 
 preds = inference(best_model, X_test)
 precision, recall, f1 = compute_model_metrics(y_test, preds)
-logger. info(f"precision: {precision}, recall: {recall}, f1: {f1}")
+logger.info(f"precision: {precision}, recall: {recall}, f1: {f1}")
