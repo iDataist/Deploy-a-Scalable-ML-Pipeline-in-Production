982ac531d7783cc2c0015d9b36892088ee30add8
Hui Ren <huiren16@gmail.com>
2021-07-25 18:09:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from pydantic import BaseModel
 import joblib
 import logging
 import pandas as pd
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 logging.basicConfig(
     level=logging.INFO,
