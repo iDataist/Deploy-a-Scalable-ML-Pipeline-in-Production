ec12a60f388063998f372727c816c2e5a9063e5f
Hui Ren <huiren16@gmail.com>
2021-07-25 17:37:52 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,26 +1,28 @@
-data_path = '../data/census.csv'
-eda_output_path = '../results/eda/eda.html'
-image_output_path = '../results/images/'
-model_output_path = '../models/rfc_model.pkl'
-log_path = '../logs/train_model.log'
+data_path = "../data/census.csv"
+processed_data_path = "../data/processed_data.csv"
+eda_output_path = "../results/eda/eda.html"
+image_output_path = "../results/images/"
+model_output_path = "../models/rfc_model.pkl"
+log_path = "../logs/train_model.log"
 cat_columns = [
-    'workclass',
-    'education',
-    'marital-status',
-    'occupation',
-    'relationship',
-    'race',
-    'sex',
-    'native-country',
-    'salary'
+    "workclass",
+    "education",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native_country",
+    "salary",
 ]
 quant_columns = [
-    'age',
-    'fnlgt',
-    'education-num',
-    'capital-gain',
-    'capital-loss',
-    'hours-per-week']
-target = 'salary_>50K'
+    "age",
+    "fnlgt",
+    "education_num",
+    "capital_gain",
+    "capital_loss",
+    "hours_per_week",
+]
+target = "salary_>50K"
 random_state = 0
 test_size = 0.3
